56c3a16247de8ab86ef1f816e53f666eb07ff27c
Philippe Loctaux <p@philippeloctaux.com>
2020-11-02 00:45:25 +0100

    tree: functions to add corner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use std::fs;
 
+fn add_corner_current_line(str: &mut String, cur_elem: usize, num_elements: usize) {
+    if cur_elem + 1 == num_elements {
+        str.push_str("└── ");
+    } else {
+        str.push_str("├── ");
+    }
+}
+
+fn add_corner_other_line(str: &mut String, cur_elem: usize, num_elements: usize) {
+    if cur_elem + 1 == num_elements {
+        str.push(' ');
+    } else {
+        str.push('|');
+    }
+    str.push_str("   ");
+}
+
 fn tree(path: &str, string: &mut String) {
     let mut cur_elem: usize = 0;
     let num_elements = fs::read_dir(path).unwrap().count();
@@ -7,35 +24,26 @@ fn tree(path: &str, string: &mut String) {
     for entry in fs::read_dir(path).unwrap() {
         let dir = entry.unwrap();
         let current_path = dir.path();
-        let current_filename = dir.file_name();
-        let current_filename = current_filename.to_str().unwrap();
+        let current_name = dir.file_name();
+        let current_name = current_name.to_str().unwrap();
 
         // if current path is a hidden folder / file, skip it
-        if current_filename.starts_with(".") {
+        if current_name.starts_with(".") {
             cur_elem += 1;
             continue;
         }
 
         // if next element is the last
-        if cur_elem + 1 == num_elements {
-            string.push_str("└── ");
-        } else {
-            string.push_str("├── ");
-        }
+        add_corner_current_line(string, cur_elem, num_elements);
 
-        println!("{}{}", string, current_filename);
+        println!("{}{}", string, current_name);
         for _ in 0..4 {
             string.pop();
         }
         if current_path.is_dir() {
             // if next element is the last
-            if cur_elem + 1 == num_elements {
-                string.push(' ');
-            } else {
-                string.push('|');
-            }
+            add_corner_other_line(string, cur_elem, num_elements);
 
-            string.push_str("   ");
             tree(current_path.to_str().unwrap(), string);
             for _ in 0..4 {
                 string.pop();
