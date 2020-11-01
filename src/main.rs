c1a9dffab2c452fa2459031c8b3910492ce654f9
Philippe Loctaux <p@philippeloctaux.com>
2020-11-02 00:18:02 +0100

    tree: simplifying logic and lil bit of refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,47 @@
 use std::fs;
 
 fn tree(path: &str, string: &mut String) {
-    let mut cur_len: usize = 0;
-    let len_folders = fs::read_dir(path).unwrap().count();
+    let mut cur_elem: usize = 0;
+    let num_elements = fs::read_dir(path).unwrap().count();
 
     for entry in fs::read_dir(path).unwrap() {
         let dir = entry.unwrap();
         let current_path = dir.path();
+        let current_filename = dir.file_name();
+        let current_filename = current_filename.to_str().unwrap();
 
         // if current path is a hidden folder / file, skip it
-        if dir.file_name().to_str().unwrap().starts_with(".") {
-            cur_len += 1;
+        if current_filename.starts_with(".") {
+            cur_elem += 1;
             continue;
         }
 
-        if cur_len + 1 == len_folders {
+        // if next element is the last
+        if cur_elem + 1 == num_elements {
             string.push_str("└── ");
         } else {
             string.push_str("├── ");
         }
-        println!("{}{}", string, dir.file_name().to_str().unwrap());
+
+        println!("{}{}", string, current_filename);
         for _ in 0..4 {
             string.pop();
         }
         if current_path.is_dir() {
-            if cur_len + 1 != len_folders {
-                string.push('|');
-            } else {
+            // if next element is the last
+            if cur_elem + 1 == num_elements {
                 string.push(' ');
+            } else {
+                string.push('|');
             }
+
             string.push_str("   ");
             tree(current_path.to_str().unwrap(), string);
             for _ in 0..4 {
                 string.pop();
             }
         }
-        cur_len += 1;
+        cur_elem += 1;
     }
 }
 
