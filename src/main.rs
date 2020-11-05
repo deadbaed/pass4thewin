24634cd94af53273b5ef0ec998c4b8fd9381f540
Philippe Loctaux <p@philippeloctaux.com>
2020-11-05 17:02:17 +0100

    sync: added commit creation, file adding, getting head commit

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ use utils::qrcode::export_to_qrcode;
 use utils::sync::{create_repo, open_repo};
 use utils::tree::tree;
 
+use crate::utils::sync::{add_file, create_commit, create_initial_commit};
 use std::env;
-use std::path::PathBuf;
+use std::fs::File;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let current_path = env::current_dir().unwrap();
@@ -16,14 +19,15 @@ fn main() {
     if let Some(tree_str) = tree(&current_path) {
         println!("{}", tree_str)
     }
-    if set_clipboard(current_path_str) == true {
+    if set_clipboard(current_path_str) {
         // somehow notifications are not always sending
         send_notification("my-secure-password");
         export_to_qrcode(current_path_str);
     }
 
     // Try to open git repository, create one on failure
-    let repo_test_path = current_path_str.to_owned() + "\\test-repo";
+    // let repo_test_path = current_path_str.to_owned() + "\\test-repo";
+    let repo_test_path = "C:\\Users\\x4m3\\Desktop\\test-repo";
     let repo_test_path = PathBuf::from(repo_test_path);
     let repo = match open_repo(&repo_test_path) {
         Some(repo) => {
@@ -33,9 +37,26 @@ fn main() {
         None => match create_repo(&repo_test_path) {
             Some(repo) => {
                 println!("repo created");
+                match create_initial_commit(&repo) {
+                    Ok(_) => println!("first commit created"),
+                    Err(e) => eprintln!("failed to create first commit {}", e),
+                }
                 repo
             }
             None => panic!("could not create repo"),
         },
     };
+
+    let relative_path = Path::new("example3.txt");
+    {
+        let file_path = repo_test_path.join(relative_path);
+        let mut file = File::create(file_path).expect("Couldn't create file");
+        file.write_all(b"Hello git2").unwrap();
+    }
+
+    let tree_file_added = add_file(&repo, &relative_path).unwrap();
+    println!("added file: {:?}", tree_file_added);
+
+    let commit_id = create_commit(&repo, &tree_file_added, "added file").unwrap();
+    println!("New commit: {}", commit_id);
 }
--- src/utils/sync.rs
+++ src/utils/sync.rs
@@ -1,16 +1,64 @@
-use git2::Repository;
-use std::path::PathBuf;
+use git2::{Commit, Error, ObjectType, Oid, Repository, Tree};
+use std::path::Path;
 
-pub fn create_repo(path: &PathBuf) -> Option<Repository> {
+pub fn create_repo(path: &Path) -> Option<Repository> {
     match Repository::init(path) {
         Ok(repo) => Some(repo),
         Err(_) => None,
     }
 }
 
-pub fn open_repo(path: &PathBuf) -> Option<Repository> {
+pub fn open_repo(path: &Path) -> Option<Repository> {
     match Repository::open(path) {
         Ok(repo) => Some(repo),
         Err(_) => None,
     }
+}
+
+/// Create empty commit with no files
+pub fn create_initial_commit(repo: &Repository) -> Result<Oid, Error> {
+    let sig = repo.signature()?;
+
+    // Create empty tree to commit
+    let empty_oid = {
+        // Get repo index
+        let mut index = repo.index()?;
+
+        // Write index to tree
+        index.write_tree()?
+    };
+    let tree = repo.find_tree(empty_oid)?;
+
+    // Make commit with no parent commits (since it's the first one)
+    repo.commit(Some("HEAD"), &sig, &sig, "Initial commit", &tree, &[])
+}
+
+/// Get HEAD commit
+fn get_head_commit(repo: &Repository) -> Result<Commit, Error> {
+    let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
+    obj.into_commit()
+        .map_err(|_| Error::from_str("failed to find commit"))
+}
+
+/// Make a commit with a tree and a message to current branch
+pub fn create_commit(repo: &Repository, tree: &Tree, message: &str) -> Result<Oid, Error> {
+    let sig = repo.signature()?;
+    let parent_commit = get_head_commit(repo)?;
+
+    repo.commit(Some("HEAD"), &sig, &sig, message, tree, &[&parent_commit])
+}
+
+/// Create tree
+pub fn add_file<'a>(repo: &'a Repository, path: &Path) -> Result<Tree<'a>, Error> {
+    // Get repo index
+    let mut index = repo.index()?;
+
+    // Add path to index
+    index.add_path(path)?;
+
+    // Write index as tree
+    let oid = index.write_tree()?;
+
+    // Get tree from newly written index
+    repo.find_tree(oid)
 }
