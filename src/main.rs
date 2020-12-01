f81fb0ae9c992ce502ff5de03946b409ffa71ebe
Philippe Loctaux <p@philippeloctaux.com>
2020-12-01 23:08:23 +0100

    cli: added args to subcommands with help

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ pub mod tree;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(about = "pass for the windows platform")]
+/// pass for the windows platform
 struct CliArgs {
     password: Option<String>,
     #[structopt(subcommand)]
@@ -18,18 +18,45 @@ struct CliArgs {
 
 #[derive(Debug, StructOpt)]
 enum Command {
+    /// Initiate password store
+    Init {
+        /// Key ID of the PGP key to use
+        gpg_id: String,
+    },
     #[structopt(name = "ls")]
-    List,
-    Find,
-    Show,
-    Insert,
-    Edit,
-    Generate,
+    /// List passwords
+    List { password: Option<String> },
+    /// Find passwords matching parameter
+    Find { search: String },
+    /// Show existing password
+    Show { password: String },
+    /// Insert new password
+    Insert { password: String },
+    /// Edit existing password
+    Edit { password: String },
+    /// Generate new password
+    /// Length is xxx if unspecified FIXME: provide actual number
+    Generate {
+        password: String,
+        length: Option<usize>,
+    },
     #[structopt(name = "rm")]
-    Remove,
+    /// Delete existing password or directory
+    Remove { old_path: String, new_path: String },
+    #[structopt(name = "mv")]
+    /// Move or rename existing password or directory
+    Move { old_path: String, new_path: String },
     #[structopt(name = "cp")]
-    Copy,
-    Git,
+    /// Copy existing password or directory
+    Copy { old_path: String, new_path: String },
+    /// If the password store is a git repository, execute some git commands
+    Git(GitCommands),
+}
+
+#[derive(Debug, StructOpt)]
+enum GitCommands {
+    /// Initiate git repository
+    Init,
 }
 
 fn main() {
