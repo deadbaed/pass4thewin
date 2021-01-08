6a0df8487668c2d0fce9787c10022f48addbcb46
Philippe Loctaux <p@philippeloctaux.com>
2021-01-08 22:04:00 +0100

    insert: check if password exists and ask for overwriting depending of force flag

--- src/cmd/insert.rs
+++ src/cmd/insert.rs
@@ -1,6 +1,7 @@
 use crate::password::Password;
 use crate::settings::Settings;
 use anyhow::anyhow;
+use dialoguer::Confirm;
 
 pub fn insert(
     password_name: &str,
@@ -9,13 +10,28 @@ pub fn insert(
     force: bool,
     settings: &Settings,
 ) -> anyhow::Result<()> {
-    // Make sure password store exists
-    settings.get_password_store_path()?;
-
-    // TODO: Check if file exists and ask for overwrite (ignore if flag force is passed)
-
     // Create empty password
     let mut password = Password::default();
+
+    // Set path of password
+    password.set_filepath(settings.get_password_store_path()?, password_name);
+
+    // Check if file exists, if so ask to overwrite if force flag is not passed
+    if password.exists()
+        && !force
+        && !Confirm::new()
+            .with_prompt(format!(
+                "An entry already exists for {}. Overwrite it?",
+                password_name
+            ))
+            .default(false)
+            .show_default(true)
+            .interact()?
+    {
+        // If user says no
+        println!("Password insertion canceled.");
+        return Ok(());
+    }
 
     // Get password from terminal
     if let Err(e) = password.terminal_input(password_name, multi_line) {
